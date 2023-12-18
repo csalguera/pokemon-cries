326bb996012beeb63aeb9a25a145ad703e79fab5
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-12-18 11:05:08 -0500

    fix indentation

--- controllers/cry.js
+++ controllers/cry.js
@@ -97,7 +97,7 @@ const deleteCry = async (req, res) => {
 
 const show = async (req, res) => {
   try {
-    const cry = await Cry.findOne( {name: req.params.name })
+    const cry = await Cry.findOne({ name: req.params.name })
     return res.status(200).json(cry)
   } catch (error) {
     console.log(error);
