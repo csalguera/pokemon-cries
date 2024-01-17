1774819b17f9150bc256620c8794a9c254735434
Carlos Salguera <carlos.e.salguera@gmail.com>
2024-01-17 14:56:32 -0500

    remove dependency

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import 'dotenv/config.js'
 import express from 'express'
 import logger from 'morgan'
 import cors from 'cors'
-import formData from 'express-form-data'
 
 // database
 import './config/database.js'
@@ -18,7 +17,6 @@ const app = express()
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
-app.use(formData.parse())
 
 // mount routes
 app.use('/api/cries', criesRouter)
